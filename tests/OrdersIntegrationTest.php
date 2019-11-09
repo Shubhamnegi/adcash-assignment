0b68dffd246aa2858cc97ad7e083b812b3b97848
shubham16negi <shubham16negi@gmail.com>
2019-11-09 15:25:47 +0530

    added test case for orders

---
+++ tests/OrdersIntegrationTest.php
@@ -0,0 +1,53 @@
+<?php
+
+
+namespace App\Tests;
+
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class OrdersIntegrationTest extends WebTestCase
+{
+
+    public function testCreateOrder()
+    {
+        $client = static::createClient();
+
+        $client->request("POST", "/api/order/", [
+            "userId" => 1,
+            "productId" => 1,
+            "quantity" => 2],
+            [],
+            [
+                "HTTP_CONTENT_TYPE" => "application/json"
+            ]);
+        $this->assertResponseStatusCodeSame(200);
+    }
+
+    public function testGetOrderByUser()
+    {
+        $client = static::createClient();
+
+        $client->request("GET", "/api/order/?getby=user&id=1", [], [], ["HTTP_CONTENT_TYPE" => "application/json"]);
+        $result = $client->getResponse()->getContent();
+
+        $result = json_decode($result, true);
+        $this->assertResponseStatusCodeSame(200);
+        $this->assertArrayHasKey("body", $result);
+        $this->assertGreaterThan(0, count($result["body"]));
+    }
+
+    public function testGetOrderByProduct()
+    {
+        $client = static::createClient();
+
+        $client->request("GET", "/api/order/?getby=product&id=1", [], [], ["HTTP_CONTENT_TYPE" => "application/json"]);
+        $result = $client->getResponse()->getContent();
+
+        $result = json_decode($result, true);
+        $this->assertResponseStatusCodeSame(200);
+        $this->assertArrayHasKey("body", $result);
+        $this->assertGreaterThan(0, count($result["body"]));
+    }
+
+}
