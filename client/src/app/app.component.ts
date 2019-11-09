5a6680fa636acdca9f0e9ef9f7a4e502f5909939
shubham16negi <shubham16negi@gmail.com>
2019-11-09 23:31:09 +0530

    added intro

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,17 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { OrderInterface } from 'src/interface/OrderInterface';
 import { OrderService } from './service/order.service';
 import { CustomResponse } from 'src/interface/CustomResponse';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { HttpErrorResponse } from '@angular/common/http';
+import * as introJs from 'intro.js/intro.js';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, AfterViewInit {
   title = 'client';
   displayedColumns: string[] = ['user_name', 'product_name', 'quantity', 'total', 'created_at', 'actions'];
   datasource = new MatTableDataSource<OrderInterface>();
@@ -25,6 +27,9 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     this.listOrders();
+  }
+  ngAfterViewInit(): void {
+    introJs().start();
   }
 
   listOrders() {
